3151171ffcb120a428ddbc997fa84f1f9abb9378
unknown <bornor>
2024-12-07 07:00:14 -0600

    okayyyy

--- productreview/urls.py
+++ productreview/urls.py
@@ -22,8 +22,15 @@ from app.views import (index, aboutus, contactus, terms, privacypolicy, user_log
                        newsletter)
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('admin/', admin.site.urls),
     path("", index, name="home"),
     path("about-us/", aboutus, name="aboutus"),
